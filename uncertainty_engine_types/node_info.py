2977b6d11f3e25b35a309f5fe0593049f4f3a371
Freddy Wordingham <freddy@digilab.co.uk>
2025-02-01 17:23:19 +0000

    Updated NodeInfo definition with new metadata

--- uncertainty_engine_types/node_info.py
+++ uncertainty_engine_types/node_info.py
@@ -2,29 +2,29 @@ from typing import Any, Optional
 
 from pydantic import BaseModel
 
-from .token import Token
-
 
 class NodeInputInfo(BaseModel):
-    type: str
     label: str
+    type: str
     description: str
-    required: bool
+    required: bool = True
+    set_in_node: bool = True
     default: Optional[Any] = None
 
 
 class NodeOutputInfo(BaseModel):
-    type: str
     label: str
+    type: str
     description: str
 
 
 class NodeInfo(BaseModel):
-    type: str  # The class of the node
-    category: str  # The categories joined by a period
-    cost: int  # The cost of the node
-    token: Token  # The token type of the node
-    short_description: str  # Comes from the class docstring
-    long_description: str  # Comes from the run method docstring
+    id: str
+    version: int
+    label: str
+    category: str
+    description: str
+    long_description: str
+    cost: int
     inputs: dict[str, NodeInputInfo]
-    outputs: dict[str, NodeOutputInfo]
+    outputs: dict[str, NodeOutputInfo] = {}
