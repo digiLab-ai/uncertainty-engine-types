119ba52843f83be498715ad040d97a16edf9e636
andrw3000 <andy@digilab.co.uk>
2025-02-13 21:07:31 +0000

    Typing message.user as Literal

--- uncertainty_engine_types/message.py
+++ uncertainty_engine_types/message.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Dict, Sequence, Union
+from typing import Dict, Literal, Sequence, Union
 
 from pydantic import BaseModel
 
@@ -8,6 +8,6 @@ Value = Union[str, float, int]
 
 
 class Message(BaseModel):
-    role: str
+    role: Literal["system", "user", "assistant"]
     content: Union[str, Dict[str, Union[Value, Sequence[Value]]]]
     timestamp: datetime
