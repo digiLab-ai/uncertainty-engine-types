c60a441f3466b055f39d8bfe4342757059d47673
andrw3000 <andy@digilab.co.uk>
2025-02-13 14:27:35 +0000

    Strongly typing the content attribute

--- uncertainty_engine_types/message.py
+++ uncertainty_engine_types/message.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-from typing import Union
+from typing import Dict, Sequence, Union
 
 from pydantic import BaseModel
 
 
+Value = Union[str, float, int]
+
+
 class Message(BaseModel):
     role: str
-    content: Union[str, dict]
+    content: Union[str, Dict[str, Union[Value, Sequence[Value]]]]
     timestamp: datetime
