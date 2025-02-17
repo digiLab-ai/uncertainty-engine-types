f119131d1ff5cb752f06fb9ac3ef082dfdf91735
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-02-17 14:43:46 +0000

    Remove more buisness logic

--- uncertainty_engine_types/sql.py
+++ uncertainty_engine_types/sql.py
@@ -1,59 +1,13 @@
-from abc import ABC, abstractmethod
 from enum import Enum
-from typing import Any
 
 from pydantic import BaseModel
-from typeguard import typechecked
-
-
-class SQLDatabase(ABC):
-    @abstractmethod
-    def execute(self, query: str) -> list[tuple[Any, ...]]:
-        """
-        Execute the given query.
-
-        Parameters:
-            query (str): The SQL query to execute.
-
-        Returns:
-            str: The result of the query.
-        """
-        pass
-
-
-class PostgreSQL(SQLDatabase):
-    @typechecked
-    def __init__(
-        self, host: str, username: str, password: str, port: int, database: str
-    ):
-        import psycopg2
-
-        self.connection = psycopg2.connect(
-            host=host, user=username, password=password, port=port, database=database
-        )
-        self.cursor = self.connection.cursor()
-
-    @typechecked
-    def execute(self, query: str) -> list[tuple[Any, ...]]:
-        """
-        Execute the given query.
-
-        Parameters:
-            query (str): The SQL query to execute.
-
-        Returns:
-            str: The result of the query.
-        """
-
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
 
 
 class SQLKind(Enum):
     POSTGRES = "POSTGRES"
 
 
-class SQLManager(BaseModel):
+class SQLConfig(BaseModel):
     """
     Connection manager for SQL databases.
     """
@@ -64,21 +18,3 @@ class SQLManager(BaseModel):
     password: str
     port: int
     database: str
-
-    @typechecked
-    def connect(self) -> SQLDatabase:
-        """
-        Connect to the SQL database.
-        """
-
-        match self.kind:
-            case SQLKind.POSTGRES:
-                return PostgreSQL(
-                    host=self.host,
-                    username=self.username,
-                    password=self.password,
-                    port=self.port,
-                    database=self.database,
-                )
-            case _:
-                raise ValueError("Unsupported SQL kind")
