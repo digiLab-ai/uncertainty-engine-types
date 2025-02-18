a0170fcda664103e9ea099cb1717798ba5e28497
Jamie Donald-McCann <61426038+JDonaldM@users.noreply.github.com>
2025-02-18 10:14:41 +0000

    `PB-192`: Separate the types from the helper logic (#11)
    * Move helper methods and functions to `uncertainty-engine-utils`.
    
    * Remove pandas and typeguard as dependencies as they are no longer needed.
    
    * Rename SQLManager to SQLConfig
    
    * Rename VectorStoreManager to VectorStoreConfig
    
    * In LLMConfig rename url to ollama_url and make it optional
    
    * In LLMConfig rename api_key to openai_api_key and make it optional
    
    ---------
    
    Co-authored-by: andrw3000 <andy@digilab.co.uk>

--- uncertainty_engine_types/tabular_data.py
+++ uncertainty_engine_types/tabular_data.py
@@ -1,13 +1,5 @@
-from io import StringIO
-
 from pydantic import BaseModel
 
 
 class TabularData(BaseModel):
     csv: str
-
-    def load_dataframe(self):
-
-        import pandas as pd
-
-        return pd.read_csv(StringIO(self.csv))
