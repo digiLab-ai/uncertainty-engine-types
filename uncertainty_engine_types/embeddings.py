1054a0d3e0cb0024bb0c992b0f8e8da8d5b38af5
andrw3000 <andy@digilab.co.uk>
2025-02-21 23:00:49 +0000

    Adding field validation to url and api key for different providers

--- uncertainty_engine_types/embeddings.py
+++ uncertainty_engine_types/embeddings.py
@@ -1,7 +1,7 @@
 from enum import StrEnum
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class TextEmbeddingsProvider(StrEnum):
@@ -18,3 +18,19 @@ class TextEmbeddingsConfig(BaseModel):
     model: Optional[str] = None
     ollama_url: Optional[str] = None
     openai_api_key: Optional[str] = None
+
+    @field_validator("ollama_url", "openai_api_key", mode="before")
+    @classmethod
+    def check_provider(cls, v, values, field):
+        provider = values.get("provider")
+        if v is None:
+            if provider == TextEmbeddingsProvider.OLLAMA and field.name == "ollama_url":
+                raise ValueError("ollama_url must be provided for 'ollama' provider.")
+            if (
+                provider == TextEmbeddingsProvider.OPENAI
+                and field.name == "openai_api_key"
+            ):
+                raise ValueError(
+                    "openai_api_key must be provided for 'openai' provider."
+                )
+        return v
