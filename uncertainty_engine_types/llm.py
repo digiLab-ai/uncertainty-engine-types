5a4fd193bb65bb4b82175a76a6ee7b72f841f04a
andrw3000 <andy@digilab.co.uk>
2025-02-24 15:50:33 +0000

    Adding model validation to LLMConfig + StrEnum to LLM provider

--- uncertainty_engine_types/llm.py
+++ uncertainty_engine_types/llm.py
@@ -1,10 +1,10 @@
-from enum import Enum
+from enum import StrEnum
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 
-class LLMProvider(Enum):
+class LLMProvider(StrEnum):
     OPENAI = "openai"
     OLLAMA = "ollama"
 
@@ -19,3 +19,13 @@ class LLMConfig(BaseModel):
     temperature: float = 0.0
     ollama_url: Optional[str] = None
     openai_api_key: Optional[str] = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def check_provider(cls, values):
+        provider = values.get("provider")
+        if provider == LLMProvider.OLLAMA and not values.get("ollama_url"):
+            raise ValueError("ollama_url must be provided for 'ollama' provider.")
+        if provider == LLMProvider.OPENAI and not values.get("openai_api_key"):
+            raise ValueError("openai_api_key must be provided for 'openai' provider.")
+        return values
