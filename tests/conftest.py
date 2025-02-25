58db3f5e973063c7022388ad22962e32c106fea7
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-02-25 14:03:02 +0000

    Add fixtures for Context, NodeInfo, NodeInputInfo, and NodeOutputInfo

---
+++ tests/conftest.py
@@ -0,0 +1,71 @@
+import pytest
+
+
+@pytest.fixture
+def node_input_info_data():
+    """
+    Some data to define a NodeInputInfo object
+    """
+
+    return {
+        "type": "type",
+        "label": "label",
+        "description": "description",
+        "required": True,
+        "set_in_node": True,
+        "default": None,
+    }
+
+
+@pytest.fixture
+def node_output_info_data():
+    """
+    Some data to define a NodeOutputInfo object
+    """
+
+    return {
+        "type": "type",
+        "label": "label",
+        "description": "description",
+    }
+
+
+@pytest.fixture
+def node_info_data(node_input_info_data, node_output_info_data):
+    """
+    Some data to define a NodeInfo object
+    """
+
+    return {
+        "id": "id",
+        "label": "label",
+        "category": "category",
+        "description": "description",
+        "long_description": "long_description",
+        "image_name": "image_name",
+        "cost": 10,
+        "inputs": {"input_1": node_input_info_data},
+        "outputs": {"output_1": node_output_info_data},
+        "load_balancer_url": "load_balancer_url",
+        "queue_url": "queue_url",
+        "cache_url": "cache_url",
+        "version_types_lib": "version_types_lib",
+        "version_base_image": 1,
+        "version_node": 1,
+    }
+
+
+@pytest.fixture
+def context_data(node_info_data):
+    """
+    Data to define a Context object
+    """
+
+    return {
+        "sync": True,
+        "job_id": "job_id",
+        "queue_url": "queue_url",
+        "cache_url": "cache_url",
+        "timeout": 60,
+        "nodes": {"node_1": node_info_data},
+    }
