5d4b329d3d63c94a3348e432a1d913807c496e99
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-02-26 17:01:12 +0000

    Add tests for the NodeInfo type

--- tests/test_node_info.py
+++ tests/test_node_info.py
@@ -4,6 +4,7 @@ import pytest
 from pydantic import ValidationError
 
 from uncertainty_engine_types import NodeInfo, NodeInputInfo, NodeOutputInfo
+from uncertainty_engine_types.version import __version__
 
 
 def test_machine_learning_model(node_input_info_data: dict):
@@ -92,3 +93,78 @@ def test_node_output_info_raise_missing(node_output_info_data: dict, field: str)
     # Try to instantiate a NodeOutputInfo object with a missing required field
     with pytest.raises(ValidationError):
         NodeOutputInfo(**node_output_info_data)
+
+
+def test_node_info(node_info_data: dict):
+    """
+    Basic test that NodeInfo model is working as expected.
+
+    Args:
+        node_info_data: Some data to define a NodeInfo object
+    """
+
+    # Instantiate a NodeInfo object
+    node_info = NodeInfo(**node_info_data)
+
+    assert node_info.model_dump() == node_info_data
+
+
+@pytest.mark.parametrize(
+    "field",
+    [
+        "id",
+        "label",
+        "category",
+        "description",
+        "long_description",
+        "image_name",
+        "cost",
+        "inputs",
+        "version_base_image",
+        "version_node",
+    ],
+)
+def test_node_info_raise_missing(node_info_data: dict, field: str):
+    """
+    Test that NodeInfo object raises an error when missing a required field.
+
+    Args:
+        node_info_data: Some data to define a NodeInfo object
+        field: A field to remove from the node_info_data
+    """
+
+    # Remove a required field
+    del node_info_data[field]
+
+    # Try to instantiate a NodeInfo object with a missing required field
+    with pytest.raises(ValidationError):
+        NodeInfo(**node_info_data)
+
+
+@pytest.mark.parametrize(
+    "field, expected",
+    [
+        ("outputs", {}),
+        ("load_balancer_url", None),
+        ("queue_url", None),
+        ("cache_url", None),
+        ("version_types_lib", __version__),
+    ],
+)
+def test_node_info_optional(node_info_data: dict, field: str, expected):
+    """
+    Test that NodeInfo object does not raise an error when missing an optional field.
+
+    Args:
+        node_info_data: Some data to define a NodeInfo object
+        field: An optional field to remove from the node_info_data
+        expected: The expected value of the field
+    """
+
+    # Remove an optional field
+    del node_info_data[field]
+
+    # Try to instantiate a NodeInfo object with a missing optional field
+    node_info = NodeInfo(**node_info_data)
+
+    assert node_info.model_dump()[field] == expected
