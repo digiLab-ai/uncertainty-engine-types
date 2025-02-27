814d8db5e95ffef7d9cf9b57fd8b0e0d00ecb19c
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-02-27 12:02:10 +0000

    Update linter script to also run flake8 on the tests. Ignore some rules with justifying comments.

--- ci/run_linter.sh
+++ ci/run_linter.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
-poetry run flake8 uncertainty_engine_types
+# When using flake8 we ignore:
+# E501: Line too long: Too strict for us
+# W503: Line break before binary operator: The advice is changing to the opposite here soon (W504), so no point. See: https://www.flake8rules.com/rules/W503.html.
+# For tests we ignore:
+# F401: When testing imports, we don't need to use the imported module
+poetry run flake8 --ignore=E501,W503 --per-file-ignores="tests/test_library_import.py:F401" uncertainty_engine_types tests
