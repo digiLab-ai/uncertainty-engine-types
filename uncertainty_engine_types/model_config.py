b6c91e701311736721df7a6bef6f59c1ad869a31
jacantwell <jasper66018@gmail.com>
2025-03-10 15:13:57 +0000

    Adding default values + changing model type to literl

--- uncertainty_engine_types/model_config.py
+++ uncertainty_engine_types/model_config.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import Optional, Literal
 
 from pydantic import BaseModel
 
 
 class ModelConfig(BaseModel):
-    train_test_ratio: float
-    input_variance: float
-    output_variance: float
-    model_type: str
+    train_test_ratio: float = 1.0
+    input_variance: Optional[float] = None
+    output_variance: Optional[float] = None
+    model_type: Literal["SingleTaskGPTorch"] = "SingleTaskGPTorch"
     seed: Optional[int] = None
