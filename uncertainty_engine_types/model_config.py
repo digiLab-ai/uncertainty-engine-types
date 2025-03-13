af0961e7b2be2fd091c17f1eee8b77b4c8395178
jacantwell <jasper66018@gmail.com>
2025-03-13 13:01:30 +0000

    Removing ttr from model config

--- uncertainty_engine_types/model_config.py
+++ uncertainty_engine_types/model_config.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 
 
 class ModelConfig(BaseModel):
-    train_test_ratio: float = 1.0
     input_variance: Optional[float] = None
     output_variance: Optional[float] = None
     model_type: Literal["SingleTaskGPTorch"] = "SingleTaskGPTorch"
