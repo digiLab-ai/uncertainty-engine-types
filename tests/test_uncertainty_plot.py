26b9a71102043e52e99cbac398fac32bf65bbbc1
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-03-18 09:50:01 +0000

    Update comments and docstring in tests

--- tests/test_uncertainty_plot.py
+++ tests/test_uncertainty_plot.py
@@ -6,13 +6,13 @@ from uncertainty_engine_types import UncertaintyPlot
 
 def test_uncertainty_plot(uncertainty_plot_data: dict):
     """
-    Basic test that ResourceID model is working as expected.
+    Basic test that UncertaintyPlot model is working as expected.
 
     Args:
-        resource_id_data: Some data to define a ResourceID object
+        uncertainty_plot_data: Some data to define a UncertaintyPlot object
     """
 
-    # Instantiate a ResourceID object
+    # Instantiate a UncertaintyPlot object
     uncertainty_plot = UncertaintyPlot(**uncertainty_plot_data)
 
     assert uncertainty_plot.model_dump() == uncertainty_plot_data
@@ -20,32 +20,32 @@ def test_uncertainty_plot(uncertainty_plot_data: dict):
 
 def test_uncertainty_plot_raise_missing(uncertainty_plot_data: dict):
     """
-    Test that ResourceID object raises an error when missing a required field.
+    Test that UncertaintyPlot object raises an error when missing a required field.
 
     Args:
-        uncertainty_plot_data: Some data to define a ResourceID object
+        uncertainty_plot_data: Some data to define a UncertaintyPlot object
     """
 
     # Remove a required field
     del uncertainty_plot_data["mean"]
 
-    # Try to instantiate a ResourceID object with a missing required field
+    # Try to instantiate a UncertaintyPlot object with a missing required field
     with pytest.raises(ValidationError):
         UncertaintyPlot(**uncertainty_plot_data)
 
 
 def test_uncertainty_plot_raise_wrong_length(uncertainty_plot_data: dict):
     """
-    Test that ResourceID object raises an error when missing a required field.
+    Test that UncertaintyPlot object raises an error when missing a required field.
 
     Args:
-        uncertainty_plot_data: Some data to define a ResourceID object
+        uncertainty_plot_data: Some data to define a UncertaintyPlot object
     """
 
     # Remove a required field
     _uncertainty_plot_data = uncertainty_plot_data.copy()
     _uncertainty_plot_data["x_vals"] = [1., 2.]  # Missing the final value
 
-    # Try to instantiate a ResourceID object with a missing required field
+    # Try to instantiate a UncertaintyPlot object with a missing required field
     with pytest.raises(ValidationError):
         UncertaintyPlot(**_uncertainty_plot_data)
