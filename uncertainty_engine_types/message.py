0e9eac90ed0a5f3ed56bcdabaa1ad1791e84b05d
Jamie Donald-McCann <jamie@digilab.co.uk>
2025-03-20 10:59:45 +0000

    Add field validator for content that stringifies content passed by the user if it is a float, int, or bool primative type.

--- uncertainty_engine_types/message.py
+++ uncertainty_engine_types/message.py
@@ -1,10 +1,16 @@
 from datetime import datetime
-from typing import Literal
+from typing import Literal, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class Message(BaseModel):
     role: Literal["instruction", "user", "engine"]
     content: str
     timestamp: datetime
+
+    @field_validator("content", mode="before")
+    @classmethod
+    def convert_content_to_string(cls, value: Union[str, int, float, bool]) -> str:
+        """Converts the content to a string if it is not already a string."""
+        return str(value)
