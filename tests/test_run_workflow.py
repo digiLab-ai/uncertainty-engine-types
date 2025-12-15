eb2ede7a9fd6c2eeaca33bd572330c2e57a92916
Nathan Creaser <nathancreaser88@gmail.com>
2025-12-15 14:31:18 +0000

    add tests for OverrideWorkflowOuput in test_run_workflows

--- tests/test_run_workflow.py
+++ tests/test_run_workflow.py
@@ -3,7 +3,7 @@ from typing import Any
 import pytest
 from pydantic import ValidationError
 
-from uncertainty_engine_types import OverrideWorkflowInput
+from uncertainty_engine_types import OverrideWorkflowInput, OverrideWorkflowOutput
 
 
 def test_override_workflow_input(override_workflow_input_data: dict[str, Any]):
@@ -37,6 +37,42 @@ def test_override_workflow_input_raise_missing(
     # Remove a required field
     del override_workflow_input_data[field]
 
-    # Try to instantiate a Message object with a missing required field
+    # Try to instantiate a OverrideWorkflowInput object with a missing required field
     with pytest.raises(ValidationError):
         OverrideWorkflowInput(**override_workflow_input_data)
+
+
+def test_override_workflow_output(override_workflow_output_data: dict[str, str]):
+    """
+    Basic test that OverrideWorkflowOutput is working as expected
+
+    Args:
+        override_workflow_output_data: data to define a OverrideWorkflowOutput
+            object
+    """
+
+    override_workflow_output = OverrideWorkflowOutput(**override_workflow_output_data)
+
+    assert override_workflow_output.model_dump() == override_workflow_output_data
+
+
+@pytest.mark.parametrize("field", ["node_label", "output_handle", "output_label"])
+def test_override_workflow_output_raise_missing(
+    override_workflow_output_data: dict[str, str], field: str
+):
+    """
+    Test that OverrideWorkflowOutput object raises an error when
+    missing a required field.
+
+    Args:
+        override_workflow_input_data: data to define a OverrideWorkflowOutput
+            object
+        field: A field to remove from the override_workflow_output_data
+    """
+
+    # Remove a required field
+    del override_workflow_output_data[field]
+
+    # Try to instantiate a OverrideWorkflowOutput object with a missing required field
+    with pytest.raises(ValidationError):
+        OverrideWorkflowOutput(**override_workflow_output_data)
