7a4f0b145d642b502dcbbd0f02868edb0bef2971
natalietf <natalie.tripp.foulds@gmail.com>
2026-03-09 13:46:21 +0000

    Update ModelConfig to include custom_config discriminator field

--- uncertainty_engine_types/model_config.py
+++ uncertainty_engine_types/model_config.py
@@ -7,14 +7,40 @@ ModelType = Literal[
     "SingleTaskGPTorch",
     "SingleTaskVariationalGPTorch",
 ]
+"""The type of model to use."""
 
 
 class ModelConfig(BaseModel):
+    """
+    The configuration for training a model in the Uncertainty Engine.
+    """
+
+    custom_config: Literal[False] = False
+    """
+    Indicates this is not a custom config. Will always be `False` for
+    `ModelConfig`.
+    """
+
     input_variance: Optional[float] = None
+    """Amount of variance to retain in input data."""
+
     input_retained_dimensions: Optional[int] = None
+    """Number of dimensions to retain in input data."""
+
     output_variance: Optional[float] = None
+    """"Amount of variance to retain in output data."""
+
     output_retained_dimensions: Optional[int] = None
+    """Number of output dimensions to retain in output data."""
+
     model_type: ModelType = "SingleTaskGPTorch"
+    """The type of model to use."""
+
     kernel: Optional[str] = None
+    """The covariance kernel."""
+
     warp_inputs: bool = False
+    """Whether to warp inputs."""
+
     seed: Optional[int] = None
+    """Seed for reproducibility."""
